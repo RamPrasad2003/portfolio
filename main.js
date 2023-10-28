f45893d3f6087dbee81bba22e45c0f843e4a2a4c
RamPrasad Chippada <140155949+RamPrasad2003@users.noreply.github.com>
2023-10-28 18:49:55 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text",{
-    strings: ["Web Developer", "AI/ML Researcher ","Data Scientist"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-});
